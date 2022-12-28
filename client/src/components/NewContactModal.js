c58031bdb98a3b58fb06cd6022dedcb3a3772a41
sgednethgirao <edneth@sheetgo.com>
2022-12-28 19:52:39 +0100

    creating new contact modal

---
+++ client/src/components/NewContactModal.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import {
+    ModalHeader,
+    ModalBody,
+    Form,
+    FormGroup,
+    Input,
+    Label,
+} from 'reactstrap'
+
+export default function NewContactModal({ closeModal }) {
+    const [contactId, setContactId] = useState('')
+    const [contactName, setContactName] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        // createContact(contactId, contactName)
+        closeModal()
+    }
+    return (
+        <>
+            <ModalHeader toggle={closeModal}> Create contact </ModalHeader>
+            <ModalBody>
+                <Form onSubmit={handleSubmit}>
+                    <FormGroup>
+                        <Label>Id</Label>
+                        <Input
+                            type="text"
+                            onChange={(e) => setContactId(e.target.value)}
+                            value={contactId}
+                            required
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Name</Label>
+                        <Input
+                            type="text"
+                            onChange={(e) => setContactName(e.target.value)}
+                            value={contactName}
+                            required
+                        />
+                    </FormGroup>
+                    <button className='btn btn-primary'>Create</button>
+                </Form>
+            </ModalBody>
+        </>
+    )
+}
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react'
 import classnames from 'classnames'
-import { TabContent, NavItem, NavLink, Nav, TabPane, Button } from 'reactstrap'
+import { TabContent, NavItem, NavLink, Nav, TabPane, Modal } from 'reactstrap'
 import Conversations from './Conversations'
 import Contacts from './Contacts'
+import NewConversationModal from './NewConversationModal'
+import NewContactModal from './NewContactModal'
 
 const CONVERSATIONS_KEY = 'conversations'
 const CONTACTS_KEY = 'contacts'
 
 export default function Sidebar({ id }) {
     const [activeTab, setActiveTab] = useState(CONVERSATIONS_KEY)
+    const [modalOpen, setModalOpen] = useState(false)
+    const conversationsOpen = activeTab === CONVERSATIONS_KEY
+
+    const closeModal = () => {
+        setModalOpen(false)
+    }
 
     return (
         <div
-            style={{ width: '250px' }}
+            style={{ width: '250px', borderRight: '1px solid #dee2e6' }}
             className="d-flex flex-column border-right h-100"
         >
             <Nav className="justify-content-center" tabs>
@@ -39,18 +47,43 @@ export default function Sidebar({ id }) {
                     </NavLink>
                 </NavItem>
             </Nav>
-            <TabContent activeTab={activeTab} style={{ flex: 1, overflow: 'auto', display: 'flex', flexDirection: 'column' }}>
+            <TabContent
+                activeTab={activeTab}
+                style={{
+                    flex: 1,
+                    overflow: 'auto',
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
                 <TabPane className="flex-grow-1" tabId={CONVERSATIONS_KEY}>
                     <Conversations />
                 </TabPane>
                 <TabPane className="flex-grow-1" tabId={CONTACTS_KEY}>
                     <Contacts />
                 </TabPane>
-                <div className='p-2 border-top border-right small'>
-                    Your Id: <span className='text-muted'>{id}</span>
+                <div
+                    className="p-2 border-top border-right small"
+                    style={{ borderRight: '1px solid #dee2e6' }}
+                >
+                    Your Id: <span className="text-muted">{id}</span>
                 </div>
-                {/* <button type="button" /> */}
+                <button
+                    onClick={() => setModalOpen(true)}
+                    type="button"
+                    className="btn btn-primary"
+                    style={{ borderRadius: '0px' }}
+                >
+                    New {conversationsOpen ? 'convertation' : 'contact'}{' '}
+                </button>
             </TabContent>
+            <Modal isOpen={modalOpen} toggle={closeModal}>
+                {conversationsOpen ? (
+                    <NewConversationModal closeModal={closeModal} />
+                ) : (
+                    <NewContactModal closeModal={closeModal} />
+                )}
+            </Modal>
         </div>
     )
 }
